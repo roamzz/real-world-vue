16641de0dee60de28bb8c74f29c88177d5fc4552
Irini Pantopoulou <ipantopoulou@draxis.gr>
2020-04-17 00:45:35 +0300

    9: api calls with axios

--- src/plugins/EventService.js.js
+++ src/plugins/EventService.js.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
-//export default axios.create({
-//  baseURL: 'http://localhost:3000'
-//});
-
-export default axios.create({
+const apiClient = axios.create({
   baseURL: `http://localhost:3000`,
   withCredentials: false, // This is the default
   headers: {
@@ -12,3 +8,12 @@ export default axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+export default {
+  getEvents() {
+    return apiClient.get('/events');
+  },
+  getEvent(id) {
+    return apiClient.get('/events/' + id);
+  },
+};
