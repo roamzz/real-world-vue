826c98d5612d251e22ff35dff0cf402fc3a953b0
Irini Pantopoulou <ipantopoulou@draxis.gr>
2020-04-16 22:11:53 +0300

    global scss

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,13 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $purple;
 }
 
 #nav {
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,9 @@
-const path = require("path");
-
 module.exports = {
-  pluginOptions: {
-    "style-resources-loader": {
-      preProcessor: "scss",
-      patterns: [path.resolve(__dirname, "./src/styles/index.scss")]
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "@/styles/_variables.scss";`
+      }
     }
   }
 };
