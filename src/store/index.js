4adfd4d067d18ab73cebdf52a789dba1b36948be
Irini Pantopoulou <ipantopoulou@draxis.gr>
2020-04-17 14:09:42 +0300

    state & Getters intro

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,27 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: {
+       id: 'abc123',
+       name: 'Irene'
+     },
+    categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community']
+
+},
   mutations: {},
   actions: {},
+    getters: {
+    catLength : state => {
+        return state.categories.length;
+    },
+    doneTodos: state => {
+        return state.todos.filter( todo => todo.done );
+    },
+    activeTodosCount: (state) => {
+    return state.todos.filter( todo => !todo.done).length;
+
+ }
+},
   modules: {}
 });
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,9 +1,31 @@
 <template>
-  <h1>Event create</h1>
+  <div>
+    <h1>Create an event {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <ol>
+      <li v-for="(cat, index) in categories" :key="index">
+        {{ cat }}
+      </li>
+    </ol>
+    <small>There are {{ catLength }} categories.</small>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapState } from "vuex";
+export default {
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength;
+    },
+    ...mapState(["categories", "user"]) // <-- using object spread operator
+  }
+  //computed: mapState({
+  //   user: 'user',
+  //   categories:'categories'
+  // })
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+</style>
