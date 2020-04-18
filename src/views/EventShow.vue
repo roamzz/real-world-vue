a1e9f50969a78a1c4593ceb6f8365e4a1558dc04
Irini Pantopoulou <ipantopoulou@draxis.gr>
2020-04-18 13:11:55 +0300

    Next Level vue-in component route guards

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -29,23 +29,31 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import { mapState } from "vuex";
+import NProgress from "nprogress";
+import store from "@/store/index.js";
+
 export default {
   props: ["id"],
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    NProgress.start(); // Start the progress bar
+    store.dispatch("event/fetchEvent", routeTo.params.id).then(() => {
+      NProgress.done(); // When the action is done complete progress bar
+      next(); // Only once this is called does the navigation continue
+    });
+  },
 
   data() {
-    return {
-    };
+    return {};
   },
 
-  created() {
-    this.fetchEvents(this.id)
-  },
-computed: mapState({
+//  created() {
+//    this.fetchEvents(this.id);
+//  },
+  computed: mapState({
     event: state => state.event.event
-}),
-    methods: mapActions('event', ['fetchEvents']),
-
+  }),
+//  methods: mapActions("event", ["fetchEvents"])
 };
 </script>
 
